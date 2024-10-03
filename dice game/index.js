8e232f950bf315de765191840492e9e324b878a7
Karthik Pulusu <146221006+KarthikPulusu@users.noreply.github.com>
2024-10-03 23:38:07 +0530

    Update index.js

--- dice game/index.js
+++ dice game/index.js
@@ -1,9 +1,9 @@
 var random_Number1=Math.floor(Math.random()*6)+1;
-var random_imagesrc="images/dice"+random_Number1+".png";
+var random_imagesrc="dice"+random_Number1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",random_imagesrc);
 
 var random_Number2=Math.floor(Math.random()*6)+1;
-var random_imagesrc2="images/dice"+random_Number2+".png";
+var random_imagesrc2="dice"+random_Number2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",random_imagesrc2);
 
 if (random_Number1>random_Number2){
